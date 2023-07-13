aaa6456bb44ee441a0d165ca5536cc237cd91e2f
Nisha <138800822+NishaSharma142@users.noreply.github.com>
2023-07-13 19:28:16 +0530

    Add files via upload

---
+++ Python_Get_started.py
@@ -0,0 +1,42 @@
+#Get started with Python
+# printing  hello World in Python
+print("Hello world")
+
+#Print("Hello World")
+
+#print(Hello world)
+
+print(10)
+
+print(10.7)
+
+#Declaring Variable in Python
+first_name="nisha"
+print(first_name)
+_name="nisha"
+print(_name)
+name1="nisha"
+print(name1)
+
+#Only allowed special character is underscore
+#Variable name cannot start with number and blank space in also not allowed 
+#1name="nisha"
+#first name="nisha"
+
+#using type() to get the data type of variable
+x=10
+print(x,type(x))
+y=5.5
+print(y,type(y))
+z="TRUE"
+print(z,type(z))
+a=True
+print(a,type(a))
+b="true"
+print(b,type(b))
+x,y=5,10
+print(y,x)
+Name="Nisha"
+print(Name,type(Name))
+Age=20
+print(Age,type(Age))
